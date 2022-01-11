45836e17b0ec603dc3c8c804e179fe736ec6bb4f
Gokulapriya-Arunachalam <91065590+Gokulapriya-Arunachalam@users.noreply.github.com>
2022-01-11 19:53:11 +0530

    Update Color_coding.h

--- Color_coding.h
+++ Color_coding.h
@@ -3,7 +3,7 @@
 
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
+const int MAX_COLORPAIR_NAME_CHARS;
 
 typedef struct {
     enum MajorColor majorColor;
