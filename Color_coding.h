20cee4e11b39f2da13dddb7608f8f10b4b35289f
Gokulapriya-Arunachalam <91065590+Gokulapriya-Arunachalam@users.noreply.github.com>
2022-01-23 20:45:03 +0530

    Update Color_coding.h

--- Color_coding.h
+++ Color_coding.h
@@ -4,11 +4,18 @@
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 const int MAX_COLORPAIR_NAME_CHARS;
+#define MaxPossibleColorsInMajorOrMinor (5u)
 
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
+
+const char* MajorColorNames[5];
+const char* MinorColorNames[5];
+
+void Print_Color_coding(void);
+void Print_ColorPairwithColorCode(int codenum, char *Majorcolor , char *Minorcolor);
 
 void testNumberToPair( int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 void testPairToNumber( enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
